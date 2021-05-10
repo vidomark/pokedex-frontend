7df5bd1db701feca385cafc41e79e7e919dd7986
Vidó Márk <vidomark42@gmail.com>
2021-05-10 17:19:18 +0200

    Header - pokemon title image

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+import imagePath from "../images/Pokemon_logo.svg";
+
+export default function Header() {
+  return (
+    <div>
+      <img src={imagePath} alt="" />
+    </div>
+  );
+}
