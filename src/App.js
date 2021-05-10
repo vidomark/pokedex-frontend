7cf57a25eed5aacbf2c2cafa0e47e071fe213f54
Zanghetsu <boythasx@gmail.com>
2021-05-10 18:29:25 +0200

    Revert "Footer -"
    This reverts commit efaf85738bc5e5af8120788d288d8d4a5bc73235.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Header from './components/Header';
-import Footer from './components/Footer';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./components/Header";
 
 function App() {
   return (
-    <div className='App'>
+    <div className="App">
       <Header />
-      <Footer />
     </div>
   );
 }
