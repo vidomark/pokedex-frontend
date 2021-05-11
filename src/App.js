22a70402721f31ab29550dcd279cbddaf0252dc7
Zanghetsu <boythasx@gmail.com>
2021-05-11 12:40:18 +0200

    Router - first and second endpoint initiated in App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
-import "./css/App.css";
-import "bootstrap/dist/css/bootstrap.min.css";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import MainComponent from "./components/MainComponent";
+import './css/App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import MainComponent from './components/MainComponent';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import React from 'react';
+import ProfilePage from './profilePageComponents/ProfilePage';
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <MainComponent />
-      <Footer />
-    </div>
+    <Router>
+      <div className='App'>
+        <Header />
+        <Route
+          exact
+          path='/'
+          render={(props) => (
+            <React.Fragment>
+              <MainComponent />
+              <Footer />
+            </React.Fragment>
+          )}
+        />
+        <Route path='profile' component={ProfilePage} />
+      </div>
+    </Router>
   );
 }
 
