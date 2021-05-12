f2ade53e417345af3c04677f6a5161afe22da972
Vidó Márk <vidomark42@gmail.com>
2021-05-12 22:26:17 +0200

    ProfilePage - previous and following pokemon choice implemented

--- src/components/PokemonController.js
+++ src/components/PokemonController.js
@@ -1,24 +1,50 @@
 import React from "react";
 import { capitalizeText } from "../util/textCapitalizer";
 import { convertId } from "../util/idConverter";
+import { useFetch } from "../hooks/useFetch";
 import { Button } from "react-bootstrap";
 
 export default function PokemonController(props) {
-  const pokemonName = props.pokemon.name;
-  const pokemonId = props.pokemon.id;
+  const previousPokemonUrl = `https://pokeapi.co/api/v2/pokemon/${
+    props.pokemon.id - 1
+  }`;
+  const followingPokemonUrl = `https://pokeapi.co/api/v2/pokemon/${
+    props.pokemon.id + 1
+  }`;
+  const [previousLoaded, previousPokemon] = useFetch(previousPokemonUrl, [
+    props.pokemon.id,
+  ]);
+  const [followingLoaded, followingPokemon] = useFetch(followingPokemonUrl, [
+    props.pokemon.id,
+  ]);
+
+  const selectPokemon = (pokemon) => {
+    props.selectPokemon(pokemon);
+  };
+
   return (
-    <div className="button-container">
-      <Button
-        variant="outline-primary"
-        className="button previous rounded-pill"
-      >
-        Primary
-      </Button>
-      <div className="pokemon-name">{capitalizeText(pokemonName)}</div>
-      <div className="pokemon-id">{convertId(pokemonId)}</div>
-      <Button variant="outline-primary" className="button next rounded-pill">
-        Primary
-      </Button>
-    </div>
+    previousLoaded &&
+    followingLoaded &&
+    previousPokemon &&
+    followingPokemon && (
+      <div className="button-container">
+        <Button
+          variant="outline-primary"
+          className="button previous rounded-pill"
+          onClick={() => selectPokemon(previousPokemon.data)}
+        >
+          {capitalizeText(previousPokemon.data.name)}
+        </Button>
+        <div className="pokemon-name">{capitalizeText(props.pokemon.name)}</div>
+        <div className="pokemon-id">{convertId(props.pokemon.id)}</div>
+        <Button
+          variant="outline-primary"
+          className="button next rounded-pill"
+          onClick={() => selectPokemon(followingPokemon.data)}
+        >
+          {followingPokemon.data.name}
+        </Button>
+      </div>
+    )
   );
 }
