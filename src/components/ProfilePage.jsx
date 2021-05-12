f78a3c49803ac43f66bcc4a4653de5897945f4c0
Zanghetsu <boythasx@gmail.com>
2021-05-12 09:41:00 +0200

    ProfilePage - picture getter better resolution

--- src/components/ProfilePage.jsx
+++ src/components/ProfilePage.jsx
@@ -18,25 +18,49 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 export default function ProfilePage(props) {
-  const pokemon = props.pokemon;
-  console.log(pokemon);
-  //const classes = useStyles();
+  const temp_json = props.pokemon;
+  console.log(temp_json);
+  const {name, sprites, id, weight, height } = props.pokemon;
+  console.log(id,name);
+  const classes = useStyles();
+
+  const getBetterPng = (id) => {
+    if (id < 10) return  `https://assets.pokemon.com/assets/cms2/img/pokedex/full/00${id}.png`;
+    if(10 <= id && id < 100 )  return `https://assets.pokemon.com/assets/cms2/img/pokedex/full/0${id}.png`;
+    return `https://assets.pokemon.com/assets/cms2/img/pokedex/full/${id}.png`;
+  }
+
+  console.log(getBetterPng);
 
   return (
-    <div>PROFILE</div>
-    /* <Grid container spacing={4} className="classes.grid">
+    <div className="main-container">
+     <Grid container spacing={4} className="classes.grid">
+      <Grid item xs={12} md={6}>
+        <Paper className={classes.Paper}>
+           <div> 
+             <img className={'profile-picture'} src={getBetterPng(id)} alt="" />
+           </div>
+        </Paper>
+      </Grid>
+      <Grid item xs={12} md={6}>
+        <Paper className={classes.Paper}>
+          <table>
+            <th>1</th>
+            <tr> 123</tr>
+            <th>2</th>
+              <tr>124</tr>
+            <th>3</th>
+            <th>4</th>
+          </table>
+        </Paper>
+      </Grid>
       <Grid item xs={12} md={6}>
         <Paper className={classes.Paper}>BRAH</Paper>
       </Grid>
       <Grid item xs={12} md={6}>
         <Paper className={classes.Paper}>BRuH</Paper>
       </Grid>
-      <Grid item xs={12} md={6}>
-        <Paper className={classes.Paper}>BRAH</Paper>
-      </Grid>
-      <Grid item xs={12} md={6}>
-        <Paper className={classes.Paper}>BRuH</Paper>
-      </Grid>
-    </Grid> */
+    </Grid> 
+    </div>
   );
 }
