c738644e356627cd54553362c6ea01b9ba7a430c
Vidó Márk <vidomark42@gmail.com>
2021-05-12 21:32:17 +0200

    ProfilePage - PokemonTable implemented

---
+++ src/components/PokemonTable.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { capitalizeText } from "../util/textCapitalizer";
+
+export default function PokemonTable({ pokemon }) {
+  return (
+    <table>
+      <tbody>
+        <tr>
+          <th>Species</th>
+          <th>Base experience</th>
+        </tr>
+        <tr>
+          <td>{capitalizeText(pokemon.species.name)}</td>
+          <td>{pokemon.base_experience}</td>
+        </tr>
+        <tr>
+          <th>Height</th>
+          <th>Weight</th>
+        </tr>
+        <tr>
+          <td>{pokemon.height}</td>
+          <td>{pokemon.weight}</td>
+        </tr>
+        <tr>
+          <th>Abilities</th>
+        </tr>
+        {pokemon.abilities.map((ability) => {
+          ability = ability.ability;
+          return (
+            <tr key={ability.name}>
+              <td>{capitalizeText(ability.name)}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+}
--- src/components/ProfilePage.jsx
+++ src/components/ProfilePage.jsx
@@ -5,6 +5,7 @@ import { convertPicture } from "../util/pictureConverter";
 import { capitalizeText } from "../util/textCapitalizer";
 import PokemonDetail from "./PokemonDetail";
 import PokemonController from "./PokemonController";
+import PokemonTable from "./PokemonTable";
 import Chart from "./Chart";
 
 const useStyles = makeStyles((theme) => ({
@@ -74,37 +75,7 @@ export default function ProfilePage(props) {
         </Grid>
         <Grid item xs={12} md={6}>
           <Paper className={classes.Paper}>
-            <table>
-              <tbody>
-                <tr>
-                  <th>Species</th>
-                  <th>Base experience</th>
-                </tr>
-                <tr>
-                  <td>{capitalizeText(pokemon.species.name)}</td>
-                  <td>{pokemon.base_experience}</td>
-                </tr>
-                <tr>
-                  <th>Height</th>
-                  <th>Weight</th>
-                </tr>
-                <tr>
-                  <td>{pokemon.height}</td>
-                  <td>{pokemon.weight}</td>
-                </tr>
-                <tr>
-                  <th>Abilities</th>
-                </tr>
-                {pokemon.abilities.map((ability) => {
-                  ability = ability.ability;
-                  return (
-                    <tr key={ability.name}>
-                      <td>{capitalizeText(ability.name)}</td>
-                    </tr>
-                  );
-                })}
-              </tbody>
-            </table>
+            <PokemonTable pokemon={pokemon} />
           </Paper>
         </Grid>
         <Grid item xs={12} md={6}>
