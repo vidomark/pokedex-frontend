9349e6476e468031d47b76b11a65ecafb0ca026c
Vidó Márk <vidomark42@gmail.com>
2021-06-20 00:30:40 +0200

    Pokemon card - complete redesign of cards

--- src/components/pokemon/PokemonCard.js
+++ src/components/pokemon/PokemonCard.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { convertPicture } from "../../util/pictureConverter";
 import { Link } from "react-router-dom";
-import { card } from "../../css/app.css";
 import PokemonDetail from "./PokemonDetail";
 
 export default function Pokemon(props) {
@@ -9,21 +8,49 @@ export default function Pokemon(props) {
     props.selectPokemon(pokemon);
   };
   const { pokemonData } = props;
+
   return (
-    <div className="card">
-      <Link
-        className="card_image"
-        to={`/pokemon/${pokemonData.id}`}
-        style={card}
-        onClick={() => selectPokemon(pokemonData)}
-      >
-        <img src={convertPicture(pokemonData.id)} alt="" />
+    <article
+      class="card card"
+      style={{ width: "200px", height: "350px", margin: "29px" }}
+      onClick={() => selectPokemon(pokemonData)}
+    >
+      <div class="card__img"></div>
+      <Link to={`/pokemon/${pokemonData.id}`} class="card_link">
+        <div
+          class="card__img--hover"
+          style={{
+            backgroundImage: `url(${convertPicture(pokemonData.id)})`,
+            width: "200px",
+            backgroundColor: "#3f3f3f",
+          }}
+        ></div>
       </Link>
-      <PokemonDetail
-        title={pokemonData.name}
-        details={pokemonData.types}
-        id={pokemonData.id}
-      />
-    </div>
+      <div class="card__info">
+        <PokemonDetail
+          title={pokemonData.name}
+          details={pokemonData.types}
+          id={pokemonData.id}
+        />
+      </div>
+    </article>
   );
+}
+
+{
+  /* <div className="card">
+  <Link
+    className="card_image"
+    to={`/pokemon/${pokemonData.id}`}
+    style={card}
+    onClick={() => selectPokemon(pokemonData)}
+  >
+    <img src={convertPicture(pokemonData.id)} alt="" />
+  </Link>
+  <PokemonDetail
+    title={pokemonData.name}
+    details={pokemonData.types}
+    id={pokemonData.id}
+  />
+</div>; */
 }
